fe6251328114df0003278b0e20fac70d4074fc7a
Heikki Orsila <heikki.orsila@iki.fi>
2005-07-08 13:15:49 +0000

    *** empty log message ***

--- src/frontends/uade123/uade-trivial.c
+++ src/frontends/uade123/uade-trivial.c
@@ -20,7 +20,7 @@ static int get_string_arg(char *dst, size_t maxlen, const char *arg, int *i,
       fprintf(stderr, "missing parameter for %s\n", argv[*i]);
       exit(-1);
     }
-    strlcpy(scorename, argv[*i + 1], sizeof(scorename));
+    strlcpy(dst, argv[*i + 1], maxlen);
     *i += 2;
     return 1;
   }
@@ -31,7 +31,8 @@ static int get_string_arg(char *dst, size_t maxlen, const char *arg, int *i,
 int main(int argc, char *argv[])
 {
   int i;
-  for (i = 1; i < argc; i++) {
+  for (i = 1; i < argc;) {
+    fprintf(stderr, "processing %s\n", argv[i]);
     if (get_string_arg(configname, sizeof(configname), "-c", &i, argv, &argc))
       continue;
     if (get_string_arg(modulename, sizeof(modulename), "-m", &i, argv, &argc))
@@ -46,13 +47,13 @@ int main(int argc, char *argv[])
     exit(-1);
   }
 
-#define CHECK_EXISTENCE(x) do { if ((x)[0] == 0) { fprintf(stderr, "must have %s\n", (x)); exit(-1); } } while (0)
+#define CHECK_EXISTENCE(x, y) do { if ((x)[0] == 0) { fprintf(stderr, "must have %s\n", (y)); exit(-1); } } while (0)
 
-  CHECK_EXISTENCE(configname);
-  CHECK_EXISTENCE(modulename);
-  CHECK_EXISTENCE(playername);
-  CHECK_EXISTENCE(scorename);
-  CHECK_EXISTENCE(uadename);
+  CHECK_EXISTENCE(configname, "config name");
+  CHECK_EXISTENCE(modulename, "module name");
+  CHECK_EXISTENCE(playername, "player name");
+  CHECK_EXISTENCE(scorename, "score name");
+  CHECK_EXISTENCE(uadename, "uade executable name");
 
   return 0;
 }
