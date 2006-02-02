08b7c31f19b1ed0cfab9d1f4c500e6ce6c0167b2
Michael Doering <mld@users.sourceforge.net>
2006-02-02 20:05:39 +0000

    *** empty log message ***

--- src/frontends/audacious/plugin.c
+++ src/frontends/audacious/plugin.c
@@ -104,9 +104,8 @@ static char gui_playername[256];
 static char gui_player_filename[PATH_MAX];
 static int last_beat_played;  /* Lock before use */
 static char md5name[PATH_MAX];
+static int out_bytes_valid; /* Lock before use */
 static int plugin_disabled;
-static int total_bytes_valid; /* Lock before use */
-static int total_bytes;       /* Lock before use */
 static struct uade_ipc uadeipc;
 static pid_t uadepid;
 static struct uade_song *uadesong;
@@ -452,12 +451,12 @@ static void *play_loop(void *arg)
 
       if (abort_playing) {
 	uade_lock();
-	total_bytes_valid = 0;
+	out_bytes_valid = 0;
 	uade_unlock();
 	break;
       }
 
-      uade_lock();
+      //uade_lock();
       if (uade_seek_forward) {
 	skip_bytes += uade_seek_forward * UADE_BYTES_PER_SECOND;
 	uade_ip.output->flush(uade_ip.output->written_time() + uade_seek_forward * 1000);
@@ -470,8 +469,8 @@ static void *play_loop(void *arg)
 	uade_select_sub = -1;
 	subsong_end = 0;
 	subsong_bytes = 0;
-	total_bytes = 0;
-	total_bytes_valid = 0;
+	uadesong->out_bytes = 0;
+	out_bytes_valid = 0;
       }
       if (subsong_end && song_end_trigger == 0) {
 	if (uadesong->cur_subsong == -1 || uadesong->max_subsong == -1) {
@@ -491,7 +490,7 @@ static void *play_loop(void *arg)
 	  }
 	}
       }
-      uade_unlock();
+      //uade_unlock();
 
       if (song_end_trigger) {
 	/* We must drain the audio fast if abort_playing happens (e.g.
@@ -539,7 +538,7 @@ static void *play_loop(void *arg)
 
 	subsong_bytes += play_bytes;
 	uade_lock();
-	total_bytes += play_bytes;
+	uadesong->out_bytes += play_bytes;
 	uade_unlock();
 
 	if (skip_bytes > 0) {
@@ -569,7 +568,7 @@ static void *play_loop(void *arg)
 	if (config.timeout != -1) {
 	  if (song_end_trigger == 0) {
 	    uade_lock();
-	    if (total_bytes / UADE_BYTES_PER_SECOND >= config.timeout)
+	    if (uadesong->out_bytes / UADE_BYTES_PER_SECOND >= config.timeout)
 	      song_end_trigger = 1;
 	    uade_unlock();
 	  }
@@ -746,8 +745,7 @@ static void uade_play_file(char *filename)
 
   abort_playing = 0;
   last_beat_played = 0;
-  total_bytes = 0;
-  total_bytes_valid = 1;
+  out_bytes_valid = 1;
 
   uade_is_paused = 0;
   uade_select_sub = -1;
@@ -827,8 +825,6 @@ static void uade_play_file(char *filename)
 
 static void uade_stop(void)
 {
-  int play_time;
-
   /* Signal other subsystems to proceed to finished state as soon as possible
    */
   abort_playing = 1;
@@ -842,17 +838,17 @@ static void uade_stop(void)
   uade_gui_close_subsong_win();
 
   if (uadesong != NULL) {
+    int play_time;
     /* If song ended volutarily, tell the play time for XMMS. */
     uade_lock();
     play_time = uadesong->playtime;
-    if (total_bytes_valid) {
-      play_time = (((int64_t) total_bytes) * 1000) / UADE_BYTES_PER_SECOND;
+    if (out_bytes_valid) {
+      play_time = (uadesong->out_bytes * 1000) / UADE_BYTES_PER_SECOND;
       if (uadesong->md5[0] != 0)
 	uade_add_playtime(uadesong->md5, play_time, 1);
-	uade_ip.set_info(gui_filename, play_time, UADE_BYTES_PER_SECOND,
-						  UADE_FREQUENCY,
+        uade_ip.set_info(gui_filename, play_time, UADE_BYTES_PER_SECOND, 
+	                                          UADE_FREQUENCY,
 						  UADE_CHANNELS);
-
     }
     uade_unlock();
 
