784d949236c9d6f95fac559315a9e4d4429ea3ca
Heikki Orsila <heikki.orsila@iki.fi>
2006-02-28 23:20:14 +0000

    *** empty log message ***

--- src/frontends/xmms/plugin.c
+++ src/frontends/xmms/plugin.c
@@ -486,7 +486,10 @@ static void *play_loop(void *arg)
 	    uade_ip.output->flush(0);
 	    subsong_end = 0;
 	    subsong_bytes = 0;
+
+	    uade_unlock();
 	    uade_gui_subsong_changed(uadesong->cur_subsong);
+	    uade_lock();
 	  }
 	}
       }
