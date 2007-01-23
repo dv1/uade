c470f6087f9a99473a71b4d0c165b8818b793571
Heikki Orsila <heikki.orsila@iki.fi>
2007-01-23 23:13:01 +0000

    fixed memmem() replacement

--- compat/memmemrep.h
+++ compat/memmemrep.h
@@ -1,29 +1,9 @@
 #ifndef _UADE_MEMMEM_H_
 #define _UADE_MEMMEM_H_
 
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 void *memmem(const void *haystack, size_t haystacklen,
 	     const void *needle, size_t needlelen);
-
-/* Slow implementation */
-void *memmem(const void *haystack, size_t haystacklen,
-	     const void *needle, size_t needlelen)
-{
-  size_t offs;
-  unsigned char *s = (unsigned char *) haystack;
-
-  if (needlelen == 0)
-    return (void *) haystack;
-
-  for (offs = 0; (offs + needlelen) <= haystacklen; offs++) {
-    if (memcmp(s + offs, needle, needlelen) == 0)
-      return s + offs;
-  }
-
-  return NULL;
-}
 
 #endif
